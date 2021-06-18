8ba036836445a21b7907fe151c4b6f28bc86d705
The-Tech-Troll-Tube <The-Tech-Troll-Tube@users.noreply.github.com>
2021-06-18 09:49:08 +0000

    deploy: fa69499f77e050078bf718390649e426ee7f6c38

---
+++ assets/js/5787b7ad.3908c0ae.js
@@ -0,0 +1,1 @@
+(self.webpackChunkthe_tech_troll_tube_website=self.webpackChunkthe_tech_troll_tube_website||[]).push([[760],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/blog/welcome"},{"title":"Hello","permalink":"/blog/hello-world"},{"title":"Hola","permalink":"/blog/hola"}]}')}}]);
---
+++ assets/js/e16015ca.cb4cd9e2.js
@@ -0,0 +1,1 @@
+(self.webpackChunkthe_tech_troll_tube_website=self.webpackChunkthe_tech_troll_tube_website||[]).push([[700],{947:function(e){"use strict";e.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/blog/tags/hola"}')}}]);
